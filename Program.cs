833a4c8f66b0f43efc1a490137879ee1a5c2e25d
LessJakub <kubafalalesniak@gmail.com>
2022-03-16 21:00:55 +0100

    Basic Azure SQL server integration added (may not be working)

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
+using DSAClinic.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
 
+//Connecting to DB
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
