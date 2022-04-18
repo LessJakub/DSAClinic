5e121488288fb4aba5dbce1474ac751b58257d26
JezewskiG <gjwsky@icloud.com>
2022-04-18 13:43:05 +0200

    Added sign-out button in account controls. Username is taken from User model.

--- client/src/app/shared/account-controls/account-controls.component.ts
+++ client/src/app/shared/account-controls/account-controls.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
+import { AccountService } from 'src/app/services/account.service';
 
 @Component({
   selector: 'app-account-controls',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountControlsComponent implements OnInit {
 
-  constructor() { }
+    user: User
 
-  ngOnInit(): void {
-  }
+    constructor(private accountService: AccountService) { }
+
+    ngOnInit(): void {}
+
+    logoutUser() {
+        this.accountService.logoutUser();
+    }
+
+    getUserName(): String {
+        if (this.user == null) {
+            this.accountService.currentUser$.subscribe(currentUser => this.user = currentUser);
+
+            if (this.user == null) {
+                return "Lucia Fernandez"
+            }
+        }
+        return this.user.username;
+    }
 
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { AccountControlsComponent } from './account-controls/account-controls.component';
 
 
-
 @NgModule({
   declarations: [
     AccountControlsComponent
