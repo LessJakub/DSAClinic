012d0b54288d33494ab417dee5928afe92fef0b5
rradarr <radorze093@student.polsl.pl>
2022-04-18 12:38:02 +0200

    Setup styling for flexbox

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  host: { 'class' : 'flex grow'}
 })
 export class LoginComponent implements OnInit {
 
